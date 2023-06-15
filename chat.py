fc984784709d5ae6559b263b71f6f5c0bf4b733d
Eric Chen <ericchen314@gmail.com>
2023-06-15 16:56:00 -0700

    Refactor code to latest version of LangChain

--- chat.py
+++ chat.py
@@ -5,6 +5,10 @@ from langchain import OpenAI, VectorDBQA
 from langchain.document_loaders import DirectoryLoader
 from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+
+from langchain.chains import RetrievalQAWithSourcesChain
+
 import os
 import nltk
 import config
@@ -26,13 +30,13 @@ loader = DirectoryLoader(config.FILE_DIR, glob='*.pdf')
 documents = loader.load()
 
 # split the text to chuncks of of size 1000
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=config.chunk_size, chunk_overlap=config.overlap)
 # Split the documents into chunks of size 1000 using a CharacterTextSplitter object
 texts = text_splitter.split_documents(documents)
 
 # Create a vector store from the chunks using an OpenAIEmbeddings object and a Chroma object
 embeddings = OpenAIEmbeddings(openai_api_key=config.OPENAI_API_KEY)
-docsearch = Chroma.from_documents(texts, embeddings)
+docsearch = Chroma.from_documents(texts, embeddings, metadatas=[{"source": f"{i}-pl"} for i in range(len(texts))])
 
 # Define answer generation function
 def answer(prompt: str, persist_directory: str = config.PERSIST_DIR) -> str:
@@ -42,17 +46,18 @@ def answer(prompt: str, persist_directory: str = config.PERSIST_DIR) -> str:
     
     # Create a prompt template using a template from the config module and input variables
     # representing the context and question.
-    prompt_template = PromptTemplate(template=config.prompt_template, input_variables=["context", "question"])
+    prompt_template = PromptTemplate(template=config.prompt_template, input_variables=["summaries", "question"])
     
     # Load a QA chain using an OpenAI object, a chain type, and a prompt template.
-    doc_chain = load_qa_chain(
+
+    doc_chain = load_qa_with_sources_chain(
         llm=OpenAI(
             openai_api_key = config.OPENAI_API_KEY,
             model_name="text-davinci-003",
             temperature=0,
-            max_tokens=300,
+            # max_tokens=config.max_tokens,
         ),
-        chain_type="stuff",
+        chain_type=config.chain_type,
         prompt=prompt_template,
     )
     
@@ -60,15 +65,17 @@ def answer(prompt: str, persist_directory: str = config.PERSIST_DIR) -> str:
     LOGGER.info(f"The top {config.k} chunks are considered to answer the user's query.")
     
     # Create a VectorDBQA object using a vector store, a QA chain, and a number of chunks to consider.
-    qa = VectorDBQA(vectorstore=docsearch, combine_documents_chain=doc_chain, k=config.k)
+    # qa = VectorDBQA(vectorstore=docsearch, combine_documents_chain=doc_chain, k=config.k)
+    retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": config.k})
+    qa = RetrievalQAWithSourcesChain(combine_documents_chain=doc_chain, retriever=retriever, return_source_documents=True)
     
     # Call the VectorDBQA object to generate an answer to the prompt.
-    result = qa({"query": prompt})
-    answer = result["result"]
+    result = qa({"question": prompt})
+    answer = result["answer"]
     
     # Log a message indicating the answer that was generated
     LOGGER.info(f"The returned answer is: {answer}")
     
     # Log a message indicating that the function has finished and return the answer.
     LOGGER.info(f"Answering module over.")
-    return answer
+    return answer, result["source_documents"]
